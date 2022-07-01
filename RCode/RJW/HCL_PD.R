8646e071ca802c456f88d26dd7aa4f7558c74876
Patrick Dolan <pdolan1110@gmail.com>
2022-07-01 10:16:28 -0400

    Update HCL_PD.R

--- RCode/RJW/HCL_PD.R
+++ RCode/RJW/HCL_PD.R
@@ -1,8 +1,12 @@
 library(tidyverse)
-#install.packages("ggbump")
+install.packages("ggbump")
 library(ggbump)
 setwd("~/Downloads/")
-inputData<-read_csv("./04_12_22_HCL3_completedata.csv")
+
+inputData<-read_csv("~/GitHub/QVEU_Code/RCode/RJW/04_12_22_HCL3_completedata.csv")
+
+inputData<-inputData %>% group_by(construct) %>% mutate(ctrlMean1= mean(Ratio1[`[PTB^-1]`==0])) %>% mutate(normVal1=Ratio1/ctrlMean1)
+inputData<-inputData %>% group_by(construct) %>% mutate(ctrlMean2= mean(Ratio2[`[PTB^-1]`==0])) %>% mutate(normVal2=Ratio2/ctrlMean2)
 
 # check correlation of Biological Reps
 
@@ -10,7 +14,7 @@ inputData<-read_csv("./04_12_22_HCL3_completedata.csv")
 modelP<-summary(with(inputData,lm(Ratio2~Ratio1)))
 
 #plot scatter plot colored by concentration of protein
-ggplot(inputData,aes(Ratio1, Ratio2))+
+ggplot(inputData,aes(Ratio1, Ratio2)) +
   geom_point(aes(col=log10(`[PTB^-1]`)))+
   geom_smooth(method = "lm")+
   ggtitle(label = paste("Biological Reps, Rsq=",round(modelP$adj.r.squared,3)))+
@@ -18,11 +22,56 @@ ggplot(inputData,aes(Ratio1, Ratio2))+
 
 #plot ratio as a function of concentration
 ggplot(inputData)+
-  geom_point(aes(`[PTB^-1]`*5, Ratio2))+
-  geom_smooth(aes(`[PTB^-1]`*5, Ratio2,group=construct,col=construct), method="glm")+
-  geom_point(aes(`[PTB^-1]`*5, Ratio1))+
-  geom_smooth(aes(`[PTB^-1]`*5, Ratio1,group=construct,col=construct), method="glm",lty=2)+
+  geom_point(aes(`[PTB^-1]`*2.9, Ratio2))+
+  geom_smooth(aes(`[PTB^-1]`*2.9, Ratio2,group=construct,col=construct), method="glm")+
+  geom_point(aes(`[PTB^-1]`*2.9, Ratio1))+
+  geom_smooth(aes(`[PTB^-1]`*2.9, Ratio1,group=construct,col=construct), method="glm",lty=2)+
   scale_x_log10()+scale_y_log10()+xlab("[PTB] (mM)")+ylab("Normalized IRES Activity\nFirefly/Renilla")
+
+#logY Relative Activity
+ggplot(inputData)+
+  theme_bw()+
+  geom_hline(yintercept = 100)+
+  geom_text(aes(1,100,label="(No PTB)"),nudge_y = .065,size=4)+
+#  geom_point(aes(`[PTB^-1]`*2.9, normVal2,col=construct))+
+  stat_summary(aes(`[PTB^-1]`*2.9,normVal2*100,col=construct),fun.y = mean,geom = "line", lwd =1)+
+  stat_summary(aes(`[PTB^-1]`*2.9,normVal2*100,col=construct),fun.data = mean_se,geom = "pointrange", lwd =1,alpha =0.4)+
+  #geom_smooth(aes(`[PTB^-1]`*2.9, normVal2,group=construct,col=construct), method="glm")+
+#  geom_point(aes(`[PTB^-1]`*2.9, normVal1,col=construct))+
+  stat_summary(aes(`[PTB^-1]`*2.9,normVal1*100,col=construct),fun.y = mean,geom = "line",lty=2,lwd=1)+
+  stat_summary(aes(`[PTB^-1]`*2.9,normVal1*100,col=construct),fun.data = mean_se,geom = "pointrange",lty=1,lwd=1,alpha=0.4)+
+  #geom_smooth(aes(`[PTB^-1]`*2.9, normVal1,group=construct,col=construct), method="glm",lty=2)+
+  scale_x_log10()+scale_y_log10()+xlab("[PTB] (mM)")+ylab("Normalized IRES Activity (%)")+scale_color_brewer(palette = "Dark2")
+
+#linearY Relative Activity
+ggplot(inputData)+
+  theme_bw()+
+  geom_hline(yintercept = 100)+
+  geom_text(aes(1,100,label="(No PTB)"),nudge_y = 8,size=4)+
+  #  geom_point(aes(`[PTB^-1]`*2.9, normVal2,col=construct))+
+  stat_summary(aes(`[PTB^-1]`*2.9,normVal2*100,col=construct),fun.y = mean,geom = "line", lwd =1)+
+  stat_summary(aes(`[PTB^-1]`*2.9,normVal2*100,col=construct),fun.data = mean_se,geom = "pointrange", lwd =1,alpha =0.4)+
+  #geom_smooth(aes(`[PTB^-1]`*2.9, normVal2,group=construct,col=construct), method="glm")+
+  #  geom_point(aes(`[PTB^-1]`*2.9, normVal1,col=construct))+
+  stat_summary(aes(`[PTB^-1]`*2.9,normVal1*100,col=construct),fun.y = mean,geom = "line",lty=2,lwd=1)+
+  stat_summary(aes(`[PTB^-1]`*2.9,normVal1*100,col=construct),fun.data = mean_se,geom = "pointrange",lty=1,lwd=1,alpha=0.4)+
+  #geom_smooth(aes(`[PTB^-1]`*2.9, normVal1,group=construct,col=construct), method="glm",lty=2)+
+  scale_x_log10()+xlab("[PTB] (mM)")+ylab("Normalized IRES Activity (%)")+scale_color_brewer(type = 'div',palette = )
+
+ggplot(inputData)+
+  geom_point(aes(`[PTB^-1]`*2.9, Ratio2/mean(inputData[construct=="negcontrol",Ratio2])))+
+  geom_smooth(aes(`[PTB^-1]`*2.9, Ratio2,group=construct,col=construct))+
+  geom_point(aes(`[PTB^-1]`*2.9, Ratio1))+
+  geom_smooth(aes(`[PTB^-1]`*2.9, Ratio1,group=construct,col=construct),lty=2)+
+  scale_x_log10()+scale_y_log10()+xlab("[PTB] (mM)")+ylab("Normalized IRES Activity\nFirefly/Renilla")
+
+ggplot(inputData)+
+  geom_point(aes(`[PTB^-1]`*2.9, Ratio2))+
+  geom_smooth(aes(`[PTB^-1]`*2.9, Ratio2,group=construct,col=construct))+
+  geom_point(aes(`[PTB^-1]`*2.9, Ratio1))+
+  geom_smooth(aes(`[PTB^-1]`*2.9, Ratio1,group=construct,col=construct),lty=2)+
+  scale_x_log10()+scale_y_log10()+xlab("[PTB] (mM)")+ylab("Normalized IRES Activity\nFirefly/Renilla")
+
 
 ggplot(inputData)+
   geom_bar(aes(`[PTB^-1]`+0.0001, FF1_adj),stat="identity", fill="lightgreen",position='dodge')+scale_x_log10()+
